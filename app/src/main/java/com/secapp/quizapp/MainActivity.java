382b99690dc2deee51800f3db32ce7ce56a592b9
Kashyap Kale <meetkashyap@outlook.com>
2019-11-22 12:13:09 +0530

    Image Chnge is not working

---
+++ app/src/main/java/com/secapp/quizapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.secapp.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button trueButton ;
+    private Button falseButton ;
+    private TextView questionview;
+    private Button next_bt;
+    private ImageView img;
+    int cnt=0;
+    private Question[] questionbank = new Question[]{
+            new Question(R.string.Question2,true,R.drawable.ic_launcher_background),
+            new Question(R.string.Question3,false,R.drawable.aarshee),
+            new Question(R.string.Question4,true,R.drawable.ic_launcher_foreground),
+            new Question(R.string.Question5,true,R.drawable.ic_launcher_foreground)
+    };
+
+//cnt
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        trueButton = findViewById(R.id.truebt);
+        falseButton = findViewById(R.id.falsebt);
+        questionview = findViewById(R.id.question);
+        img = findViewById(R.id.imageView);
+        next_bt = findViewById(R.id.nextbt);
+        trueButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+        next_bt.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.truebt:
+                Toast.makeText(MainActivity.this,"TRUE",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.falsebt:
+                Toast.makeText(MainActivity.this,"FALSE",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nextbt:
+                if(cnt<4)
+                    cnt = (cnt+1) % questionbank.length;
+                questionview.setText(questionbank[cnt].getRsid());
+                img.setImageResource(questionbank[cnt].getImgid());
+                break;
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/secapp/quizapp/Question.java
@@ -0,0 +1,38 @@
+package com.secapp.quizapp;
+
+public class Question {
+    private int rsid;
+    private boolean ans;
+    private int imgid;
+
+    public Question(int rsid, boolean ans,int imgid) {
+        this.rsid = rsid;
+        this.ans = ans;
+        this.imgid=imgid;
+    }
+
+    public int getRsid() {
+        return rsid;
+    }
+
+    public void setRsid(int rsid) {
+        this.rsid = rsid;
+    }
+
+    public void setAns(boolean ans) {
+        this.ans = ans;
+    }
+
+    public boolean getAns(){
+        return ans;
+    }
+
+    public int getImgid() {
+        return rsid;
+    }
+
+    public void setImgidid(int rsid) {
+        this.rsid = rsid;
+    }
+
+}
